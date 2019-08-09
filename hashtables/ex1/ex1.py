d03141b1fc35816c9ac9c46b8d2032d57811dc7f
Lambda_School_Loaner_62 <lambda_school_loaner_62@Lambda-School-Loaner-62s-MacBook-Pro.local>
2019-08-09 09:43:30 -0700

    add ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,7 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
 from hashtables import (HashTable,
                         hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+                        hash_table_retrieve,)
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -12,6 +10,15 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    for index in range(length):
+
+        complementary_number = limit - weights[index]
+        found = hash_table_retrieve(ht, complementary_number)
+
+        if found is not None:
+            return (index, found)
+
+        hash_table_insert(ht, weights[index],index)
 
     return None
 
